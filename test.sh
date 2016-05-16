a1463d1f13beb935d27584452c8f6e2b866bcbad
Schinder <adam.p.schinder@gmail.com>
2016-05-15 23:39:46 -0400

    Add some comments to the test.sh shell script

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Author: Adam Schinder
+# File: test.sh
+# Purpose: This bash script just shows a nifty use of the date variables available in bash.
 
 echo "this line is executed at `date +%h-%m-%s`"
 sleep 30
